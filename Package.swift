81fbc9ce07c5fe84cd993ce5f259dd51a06603ad
Darkzwer123 <137776351+Darkzwer123@users.noreply.github.com>
2024-06-05 17:24:11 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
             name: "DocumentReader",
             targets: ["DocumentReader"]),
     ],
+    dependencies: [.package(url: "https://github.com/Darkzwer/Common.git", from: "7.2.487")],
     targets: [
         .binaryTarget(name: "DocumentReader", url: "https://pods.regulaforensics.com/DocumentReader/7.2.3545/DocumentReader-7.2.3545.zip", checksum: "833b43dba07afceed74e14e8cd6c4c5fb41055da987164ee047f195f4577ab6d"),
     ]
