f844cf6fa28c221b45f74d128ccb2407dcde1e06
Darkzwer <137776351+Darkzwer123@users.noreply.github.com>
2024-08-20 15:49:02 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,13 +1,15 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+let packageName = "DocumentReader"
+
 let package = Package(
     name: "DocumentReader",
     platforms: [.iOS(.v11)],
     products: [
         .library(
             name: "DocumentReader",
-            targets: ["DocumentReaderSDKDeps"]),
+            targets: ["\(packageName)Common"]),
     ],
     dependencies: [
         .package(name: "RegulaCommon", url: "https://github.com/regulaforensics/RegulaCommon-Swift-Package.git", .exact(Version(stringLiteral: "7.1.379"))),
@@ -15,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(name: "DocumentReader", url: "https://pods.regulaforensics.com/DocumentReader/7.1.3379/DocumentReader-7.1.3379.zip", checksum: "24f3ab840a064180a0e72ad3e22bd260e2595ecd8a4ec20b2e32b01965a9f976"),
         .target(
-            name: "DocumentReaderSDKDeps",
+            name: "\(packageName)Common",
             dependencies: [
                 .target(name: "DocumentReader"),
                 .product(name: "RegulaCommon", package: "RegulaCommon")
